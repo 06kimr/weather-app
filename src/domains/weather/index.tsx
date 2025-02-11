7bd46787eda49a9e087409c35558940b075c846c
rickykim-99 <youngs.kim@creadto.com>
2025-02-11 23:55:46 +0900

    [FEAT] weather api connected

---
+++ src/domains/weather/index.tsx
@@ -0,0 +1,25 @@
+import { WeatherAdapterInterface } from "@/api/weather/types";
+import { FC } from "react";
+import { mergeForecastWithShortTermForecast } from "./utils";
+
+interface Props {
+  live: Awaited<ReturnType<WeatherAdapterInterface["live"]>>;
+  today_temperature: Awaited<
+    ReturnType<WeatherAdapterInterface["todayTemperature"]>
+  >;
+  merged_forecast: ReturnType<typeof mergeForecastWithShortTermForecast>;
+}
+
+const WeatherMain: FC<Props> = (props) => {
+  const { live, today_temperature, merged_forecast } = props;
+
+  console.log(merged_forecast);
+
+  return (
+    <main>
+      <h1>Weather</h1>
+    </main>
+  );
+};
+
+export { WeatherMain };

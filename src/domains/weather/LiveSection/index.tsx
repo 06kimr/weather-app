9dfb659cc4339936671be4050268316a4a0b7053
rickykim-99 <youngs.kim@creadto.com>
2025-02-12 21:08:34 +0900

    [FEAT] live-section

--- src/domains/weather/LiveSection/index.tsx
+++ src/domains/weather/LiveSection/index.tsx
@@ -1,5 +1,7 @@
 import { WeatherAdapterInterface } from "@/api/weather/types";
 import { FC } from "react";
+import { WindDirection } from "./WindDirection";
+import { getRainyType } from "../utils";
 
 interface Props {
   live: Awaited<ReturnType<WeatherAdapterInterface["live"]>>;
@@ -10,7 +12,53 @@ interface Props {
 
 const LiveSection: FC<Props> = (props) => {
   const { live, today_temperature } = props;
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        <div>
+          <strong>{live.T1H?.obsrValue}</strong>
+          <strong>{getRainyType(live.PTY?.obsrValue)}</strong>
+        </div>
+        <div>
+          {/* 최저, 최고 기온 */}
+          <dl>
+            <dt>최저</dt>
+            <dd>
+              <strong>{today_temperature.min}</strong>
+            </dd>
+          </dl>
+          <dl>
+            <dt>최고</dt>
+            <dd>
+              <strong>{today_temperature.max}</strong>
+            </dd>
+          </dl>
+          <span>(오전 6시, 오후 3시)</span>
+        </div>
+      </div>
+      <div>
+        <dl>
+          <dt>강수량</dt>
+          <dd>{live.RN1?.obsrValue}</dd>
+        </dl>
+        <dl>
+          <dt>습도</dt>
+          <dd>{live.REH?.obsrValue}</dd>
+        </dl>
+        <dl>
+          <dt>풍향</dt>
+          <dd>
+            {live.VEC?.obsrValue}
+            <WindDirection direction={parseInt(live.VEC?.obsrValue ?? "0")} />
+          </dd>
+        </dl>
+        <dl>
+          <dt>풍속</dt>
+          <dd>{live.WSD?.obsrValue}</dd>
+        </dl>
+      </div>
+    </section>
+  );
 };
 
 export { LiveSection };
